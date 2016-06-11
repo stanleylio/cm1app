bda3bb64fcb134e881956002fbb39be4b1e42e2c
Stanley Lio <stanleylio@gmail.com>
2016-06-11 03:07:42 +0000

    .

--- cm1app/dashboard.py
+++ cm1app/dashboard.py
@@ -71,8 +71,8 @@ def data_dashboard(site):
         elif 'coconut' == site:
             r = {'site':site,
                  'data_src':'base-002',
-                 'data_src_name':'Base Station #2 (BBB-based)',
-                 'location':'In a water tank somewhere on Coconut Island',
+                 'data_src_name':'Base Station #2 (BBB-in-a-box)',
+                 'location':'Coconut Island, Kane\'ohe',
                  'gmap_link':'https://goo.gl/maps/2YYpJ6Ru6692',
                  'nodes':S}
 
