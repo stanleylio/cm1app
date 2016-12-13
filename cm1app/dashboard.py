8e15c4cccdf47b5fb46864c517e79b049ca3916a
Stanley Lio <stanleylio@gmail.com>
2016-12-13 07:28:55 +0000

    .

--- cm1app/dashboard.py
+++ cm1app/dashboard.py
@@ -4,7 +4,6 @@ sys.path.append('/home/nuc')
 from flask import Flask,render_template,Markup,request
 from cm1app import app
 from json import dumps
-#from node.helper import dt2ts
 from node.storage.storage2 import storage_read_only as store2
 from node.config.config_support import get_list_of_nodes,get_list_of_disp_vars,\
      get_name,get_unit,get_dbfile,\
@@ -21,42 +20,50 @@ site_base_map = {'poh':'base-003',\
 
 @app.route('/<site>/data/dashboard.json')
 def data_dashboard(site):
-    if site in site_base_map.keys():
-        nodes = get_list_of_nodes(site)
-        store = store2()
-        
-        S = {}
-        for node in nodes:
-            S[node] = {}
-            S[node]['name'] = get_name(site,node)
-            S[node]['location'] = get_location(site,node)
-            S[node]['latest_non_null'] = {}
-            #print node,S[node]['name']
-            for var in get_list_of_disp_vars(site,node):
-                tmp = store.read_latest_non_null(node,time_col,var)
-                if tmp is not None:
-                    r = [tmp[time_col],tmp[var],get_unit(site,node,var)]
-                else:
-                    r = [None,None,get_unit(site,node,var)]
+    if site not in site_base_map.keys():
+        return 'Error: Unknown site: {}'.format(site)
+    
+    nodes = get_list_of_nodes(site)
+    store = store2()
+    
+    S = {}
+    for node in nodes:
+        S[node] = {}
+        S[node]['name'] = get_name(site,node)
+        S[node]['location'] = get_location(site,node)
+        S[node]['latest_non_null'] = {}
+        #print node,S[node]['name']
 
-                # put in the boundaries/limits/range of the variable, if defined
-                b = get_range(site,node,var)
-                if b is not None:
-                    b = b.to_tuple()
-                    b = [None if tmp == float('inf') else tmp for tmp in b]
-                    #r.extend(b)
-                    r.append(b)
-                #print r
-                
-                S[node]['latest_non_null'][var] = r
+        for var in get_list_of_disp_vars(site,node):
+            # [timestamp,reading,unit,[lower bound,upper bound]]
+            
+            tmp = store.read_latest_non_null(node,time_col,var)
+            if tmp is not None:
+                r = [tmp[time_col],tmp[var]]
+            else:
+                r = [None,None]
 
-        
-        base = site_base_map[site]
-        r = {'site':site,
-             'data_src':base,
-             'data_src_name':get_note(site,base),
-             'location':get_location(site,base),
-             'gmap_link':get_google_earth_link(site,base),
-             'nodes':S}
-        return dumps(r,separators=(',',':'))
-    return 'Error: Unknown site: {}'.format(site)
+            # add unit
+            r.append(get_unit(site,node,var))
+
+            # put in the boundaries/limits/range of the variable
+            # use [None,None] if the limits of a variable are not defined.
+            b = get_range(site,node,var)
+            if b is None:
+                r.append([None,None])
+            else:
+                b = b.to_tuple()
+                b = [None if tmp in [float('-inf'),float('inf')] else tmp for tmp in b]
+                r.append(b)
+            
+            S[node]['latest_non_null'][var] = r
+
+    # site info (data source, location etc.)
+    base = site_base_map[site]
+    r = {'site':site,
+         'data_src':base,
+         'data_src_name':get_note(site,base),
+         'location':get_location(site,base),
+         'gmap_link':get_google_earth_link(site,base),
+         'nodes':S}
+    return dumps(r,separators=(',',':'))
