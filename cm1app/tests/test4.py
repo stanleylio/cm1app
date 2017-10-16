37e8e42b0a3c89d4c5a30d6a13f4f2dfebb8494f
Stanley Lio <stanleylio@gmail.com>
2017-10-16 09:57:09 +0000

    .

---
+++ cm1app/tests/test4.py
@@ -0,0 +1,17 @@
+import requests
+
+
+host = 'https://grogdata.soest.hawaii.edu'
+
+
+# experimental api
+url = host + '/data/2/node-020/ReceptionTime,t0,t1.json?time_col=ReceptionTime&begin=1506070000&end=1506079094'
+r = requests.get(url)
+assert r.status_code == 200
+#r = r.json()
+#print(len(r))
+#print(len(r[0]))
+#print(r)
+
+
+
