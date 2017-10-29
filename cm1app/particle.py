df3e181960db6672993763463c46daca08538613
Stanley Lio <stanleylio@gmail.com>
2017-10-29 07:17:01 +0000

    removed hard-coded fish_map

--- cm1app/particle.py
+++ cm1app/particle.py
@@ -2,6 +2,22 @@ import sys,json,time
 sys.path.append('/home/nuc')
 from datetime import datetime,timedelta
 from node.helper import dt2ts,ts2dt
+from node.config.config_support import get_config,get_list_of_sites,get_list_of_nodes
+
+
+fish_map = {}
+
+def coreid2nodeid(coreid):
+    # WARNING: coreid is probably of type "unicode"...
+    global fish_map
+
+    if coreid not in fish_map:
+        for site in sorted(get_list_of_sites()):
+            for node in get_list_of_nodes(site):
+                tmp = get_config('coreid',node)
+                if tmp is not None:
+                    fish_map[tmp] = node
+    return fish_map.get(coreid,None)
 
 
 def parse_electron_msg(ts,s,sample_interval=60):
@@ -13,54 +29,10 @@ def parse_electron_msg(ts,s,sample_interval=60):
     return zip(t,s)
 
 def fish_handler(request):
-    # TODO: grab these from the config files
-    fish_map = {u'1f0024001751353338363036':'node-028', # Boston, Amy Mueller
-                u'450057000a51343334363138':'node-029', # Boston, Amy Mueller
-                u'280021001951353338363036':'node-045', # SF, Jim Trezzo
-                u'360064001951343334363036':'node-046', # NOAA, Coconut
-                u'410055001951353338363036':'node-047', # T/P/RH test bed
-                #u'180033001951353338363036':'node-048',
-                u'33002f001851353338363036':'node-048',
-                u'3e0042001951353338363036':'node-049', # First Makaha, PoH
-                u'2d0039001851353338363036':'node-050',
-                u'230053001951353338363036':'node-051', # Storm Makaha, PoH
-                u'4e0029001751353338363036':'node-052', # Waikalua Loko
-                #u'3a0038001751353338363036':'node-053',
-                u'23003b000351353337353037':'node-053',
-                u'4e0053001851353338363036':'node-054',
-                u'210048001851353338363036':'node-055',
-                u'2a002b001751353338363036':'node-056',
-                u'190049000251353337353037':'node-057',
-                u'40002e001951353338363036':'node-058', # Moonshot
-                u'290048001951353338363036':'node-059',
-                #u'5d003e001951353338363036':'node-060',
-                u'2b001b001951353338363036':'node-060',
-                u'4d0038001751353338363036':'node-061', # Moonshot
-                u'2d0044001851353338363036':'node-062',
-                #u'2f002f001951353338363036':'node-063',
-                u'3d0058001751353338363036':'node-063',
-                #u'420030001751353338363036':'node-064',
-                u'1e0029001751353338363036':'node-064',
-                u'2d0055001851353338363036':'node-065',
-                u'2e0056001751353338363036':'node-066',
-                u'1e002c001751353338363036':'node-067',
-                u'470032001951353338363036':'node-068',
-                u'4d0057001851353338363036':'node-069',
-                u'4b002f001951343334363036':'node-070',
-                #u'580052001951353338363036':'node-071',
-                u'230048001951353338363036':'node-071',
-                u'220026000351353337353037':'node-072',
-                u'1d0053000251353337353037':'node-073',
-                u'1f0043000251353337353037':'node-074',
-                u'25002d001951353338363036':'node-075',
-                u'40004f001951353338363036':'node-076', # Moonshot
-                u'4d0058001851353338363036':'node-077',
-                }
-
-    if request.form['coreid'] not in fish_map:
+    nodeid = coreid2nodeid(request.form['coreid'])
+    if nodeid is None:
         return None,'unknown coreid'
 
-    nodeid = fish_map[request.form['coreid']]
     if nodeid in ['node-028','node-029']:
         # firmware version p2
         published_at = request.form['published_at']
@@ -71,7 +43,7 @@ def fish_handler(request):
             d.append({'ReceptionTime':s[0],'d2w':s[1]})
         return nodeid,d
     else:
-        # firmware version p3~p5d
+        # firmware version p3~p5e
         if u'd2w' == request.form['event']:
             rt = time.time()
             d = []
@@ -89,10 +61,3 @@ def fish_handler(request):
             d = json.loads(request.form['data'])
             d['ReceptionTime'] = time.time()
             return nodeid,[d]
-
-            '''d = json.loads(request.form['data'])
-            if 'VbattV' in d:
-                d['ReceptionTime'] = time.time()
-                return nodeid,[d]
-            else:
-                return nodeid,None'''
